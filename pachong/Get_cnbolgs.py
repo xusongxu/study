f03453fc347de947e89357d105223fedb0201632
xusongxu <https://github.com/xusongxu>
2020-04-02 00:43:54 +0800

    get all cnblogs's title+link+Text content

--- pachong/Get_cnbolgs.py
+++ pachong/Get_cnbolgs.py
@@ -1,3 +1,4 @@
+"""简单获取一篇博客文章的标题和内容
 # 导入requests模块
 import requests
 import urllib.request
@@ -38,4 +39,90 @@ with open(filename,'w',encoding='utf-8') as f:
     # 在文件中添加博客文章的具体内容
     f.write(div.text)
 print(type(title_pattern))
+"""
 
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+#该作者的博文一共有多少页
+pageNo=18
+#后面需要添加页码
+url='https://www.cnblogs.com/Mr-choa/default.html?page='
+# 获取网页源码
+def get_html(url):
+    """
+    返回对应url的网页源码，经过解码的内容
+    :param url:
+    :return:
+    """
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    html_page = resp.read().decode('utf-8')
+    return html_page
+# 获取博客文章的标题
+def get_title(url):
+    '''
+    获取对应url下文章的标题
+    :param url:
+    :return:
+    '''
+    html_page = get_html(url)
+    title_pattern = r'(<a.*id="cb_post_title_url".*>)(.*)(</a>)'
+    title_match = re.search(title_pattern, html_page)
+    title = title_match.group(2)
+    return title
+# 获取博客文章的文本
+def get_Body(url):
+    """
+    获取对应url的文章的正文内容
+    :param url:
+    :return:
+    """
+    html_page = get_html(url)
+    soup = BeautifulSoup(html_page, 'html.parser')
+    div = soup.find(id="cnblogs_post_body")
+    return div.text
+# 保存文章
+def save_file(url):
+    """
+    根据url，将文章保存到本地
+    :param url:
+    :return:
+    """
+    title=get_title(url)
+    body=get_Body(url)
+    filename="Mr_choa"+'-'+title+'.txt'
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(title)
+        f.write(url)
+        f.write(body)
+# 遍历所有的博客文章链接，保存博客的文章
+def save_files(url,pageNo):
+    '''
+    根据url和pageNo，保存博主所有的文章
+    :param url:
+    :param pageNo:
+    :return:
+    '''
+    totol_urls=get_Urls(url,pageNo)
+    for url_ in totol_urls:
+        save_file(url_)
+# 获取所有的链接
+def get_Urls(url,pageNo):
+    """
+    根据url，pageNo，能够返回该博主所有的文章url列表
+    :param url:
+    :param pageNo:
+    :return:
+    """
+    total_urls=[]
+    for i in range(1,pageNo+1):
+        url_1=url+str(i)
+        html=get_html(url_1)
+        title_pattern=r'<a.*class="postTitle2".*href="(.*)">'
+        urls=re.findall(title_pattern,html)
+        for url_ in urls:
+            total_urls.append(url_)
+    #print(total_urls.__len__())
+    return total_urls
+save_files(url,pageNo)
