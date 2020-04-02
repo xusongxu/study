6af7e7cb16b00c3f618a7eba991693e9ce184466
xusongxu <https://github.com/xusongxu>
2020-04-02 23:56:34 +0800

    get all cnblogs's links + title + content

--- pachong/Get_cnbolgs.py
+++ pachong/Get_cnbolgs.py
@@ -41,11 +41,13 @@ with open(filename,'w',encoding='utf-8') as f:
 print(type(title_pattern))
 """
 
+
+import requests
 import urllib.request
 import re
 from bs4 import BeautifulSoup
 #该作者的博文一共有多少页
-pageNo=18
+pageNo=2
 #后面需要添加页码
 url='https://www.cnblogs.com/Mr-choa/default.html?page='
 # 获取网页源码
@@ -55,10 +57,25 @@ def get_html(url):
     :param url:
     :return:
     """
+    # 创建一个请求对象
     req = urllib.request.Request(url)
+    # 发起请求，urlopen返回的是一个HTTPResponse对象
     resp = urllib.request.urlopen(req)
+    # 获取HTTP源代码，编码格式为utf-8
     html_page = resp.read().decode('utf-8')
+    # 返回网页源码
     return html_page
+
+'''# 获取网页源代码
+def get_html(url):
+    # 创建一个响应对象
+    response=requests.get(url)
+    # 获取整个网页的HTML内容
+    html_page=response.text
+    # 返回网页的HTML内容
+    return html_page
+'''
+
 # 获取博客文章的标题
 def get_title(url):
     '''
@@ -66,10 +83,15 @@ def get_title(url):
     :param url:
     :return:
     '''
+    # 通过博文的地址获取到源代码
     html_page = get_html(url)
+    # 创建str变量
     title_pattern = r'(<a.*id="cb_post_title_url".*>)(.*)(</a>)'
+    # 匹配到相关的数据
     title_match = re.search(title_pattern, html_page)
+    # 获取标题
     title = title_match.group(2)
+    # 返回标题
     return title
 # 获取博客文章的文本
 def get_Body(url):
@@ -78,9 +100,13 @@ def get_Body(url):
     :param url:
     :return:
     """
+    # 通过博客文章的链接,获取博客文章的源代码
     html_page = get_html(url)
+    # 创建对象，基于bs4库HTML的格式输出
     soup = BeautifulSoup(html_page, 'html.parser')
+    # 定义一个soup进行find（）方法处理的标签
     div = soup.find(id="cnblogs_post_body")
+    # 返回博客文章内容
     return div.text
 # 保存文章
 def save_file(url):
@@ -115,14 +141,23 @@ def get_Urls(url,pageNo):
     :param pageNo:
     :return:
     """
+    # 创建一个list，用来装博客文章的地址
     total_urls=[]
+    # 对页数做个遍历
     for i in range(1,pageNo+1):
+        # 页数的地址
         url_1=url+str(i)
+        # 获取这一页的全部源代码
         html=get_html(url_1)
+        # 创建一个属性
         title_pattern=r'<a.*class="postTitle2".*href="(.*)">'
+        # 通过正则表达式找到所有相关属性的数据，就是所有的博客文章的链接
         urls=re.findall(title_pattern,html)
+        # 把链接放到list容器内
         for url_ in urls:
             total_urls.append(url_)
     #print(total_urls.__len__())
+    # 返回所有博客文章的链接
     return total_urls
 save_files(url,pageNo)
+
